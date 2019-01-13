316994f3789b321919b69d6e1177d3581b0f8a4e
Gregory Chen <gregorychen3@gmail.com>
2019-01-12 22:22:07 -0500

    Add welcome component content

--- website/src/components/Welcome.js
+++ website/src/components/Welcome.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Welcome extends Component {
   render() {
     return (
-      "Welcome"
+      <div id="home_content">
+        <img alt="welcome_pic" src="/home_pic.jpg" />
+      </div>
     )
   }
 }
