4063e5fc535e58e0174653af30007e617d6383a4
Gregory Chen <gregorychen3@gmail.com>
2019-01-12 22:38:56 -0500

    impl songbook tunes page content

--- website/src/components/MusicSongbookTunes.js
+++ website/src/components/MusicSongbookTunes.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class MusicSongbookTunes extends Component {
   render() {
     return (
-      "MusicSongbookTunes"
+      <div id="sheetmusic_content">
+        <iframe
+          title="sheetmusic-content"
+          src="https://drive.google.com/embeddedfolderview?id=1XDfbY6K4GzA3Etm-dlwN0-HGhrIICkgw#list"
+        />
+      </div>
     )
   }
 }
