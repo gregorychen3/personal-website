25d6310eb56006a0f2fefe4f3ff02ed2f102bf78
Gregory Chen <gregorychen3@gmail.com>
2019-11-07 22:16:00 -0500

    add home page pic

--- webapp/src/pages/HomePage.tsx
+++ webapp/src/pages/HomePage.tsx
@@ -3,10 +3,9 @@ import React from "react";
 export default () => (
   <section className="section">
     <div className="container">
-      <h1 className="title">Home Page</h1>
-      <p className="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
+      <figure className="image">
+        <img src="riverside_church.jpg" />
+      </figure>
     </div>
   </section>
 );
