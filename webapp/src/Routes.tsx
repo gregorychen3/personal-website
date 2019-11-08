1391d548879f912665599a2cf08eb55d6ea13c1d
Gregory Chen <gregorychen3@gmail.com>
2019-11-08 14:28:29 -0500

    add schedule page

--- webapp/src/Routes.tsx
+++ webapp/src/Routes.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router";
 import HomePage from "./pages/HomePage";
 import ListenPage from "./pages/ListenPage";
+import SchedulePage from "./pages/SchedulePage";
 
 export default () => (
   <Switch>
     <Route path="/" exact component={HomePage} />
     <Route path="/music/listen" exact component={ListenPage} />
-    <Route path="/music/schedule" exact component={HomePage} />
+    <Route path="/music/schedule" exact component={SchedulePage} />
     <Route path="/music/songbook" exact component={HomePage} />
     <Route path="/music/resume" exact component={HomePage} />
     <Route path="/software/resume" exact component={HomePage} />
---
+++ webapp/src/pages/SchedulePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default () => (
+  <section className="section">
+    <div className="container">
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=dG9hY3RtajJlaGltbGdmNWI0cnUycHB2aDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23871111&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;showTz=1"
+        width="100%"
+        height="600"
+        data-frameborder="0"
+        data-scrolling="no"
+        style={{ borderWidth: "0" }}
+      ></iframe>
+    </div>
+  </section>
+);
