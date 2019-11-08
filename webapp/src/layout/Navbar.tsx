406d87a9fb899d1f8da46198f58eb49995cd76c2
Gregory Chen <gregorychen3@gmail.com>
2019-11-07 20:29:36 -0500

    navbar item spacing

--- webapp/src/layout/Navbar.tsx
+++ webapp/src/layout/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Navbar.scss";
 
 export default () => (
   <nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
-      <a className="navbar-item" href="#/">
-        GREGORY CHEN
+      <a className="navbar-item " href="#/">
+        <div className="is-size-4">GREGORY CHEN</div>
       </a>
 
       <a
@@ -20,31 +21,26 @@ export default () => (
       </a>
     </div>
 
-    <div id="navbarBasicExample" className="navbar-menu">
-      <div className="navbar-start">
-        <div className="navbar-item has-dropdown is-hoverable">
-          <a className="navbar-link">Music</a>
-          <div className="navbar-dropdown">
-            <a className="navbar-item">Listen</a>
-            <a className="navbar-item">Schedule</a>
-            <a className="navbar-item">Songbook</a>
-            <a className="navbar-item">Resume</a>
-          </div>
-        </div>
-        <div className="navbar-item has-dropdown is-hoverable">
-          <a className="navbar-link">Software</a>
-          <div className="navbar-dropdown">
-            <a className="navbar-item">Resume</a>
-            <a className="navbar-item">LinkedIn</a>
-            <a className="navbar-item">Recipe Book Web App</a>
-            <a className="navbar-item">LiquorBuddy Mobile App</a>
-          </div>
+    <div className="navbar-end">
+      <div className="navbar-item has-dropdown is-hoverable">
+        <a className="navbar-link is-arrowless">Music</a>
+        <div className="navbar-dropdown is-right">
+          <a className="navbar-item">Listen</a>
+          <a className="navbar-item">Schedule</a>
+          <a className="navbar-item">Songbook</a>
+          <a className="navbar-item">Resume</a>
         </div>
       </div>
-
-      <div className="navbar-end">
-        <a className="navbar-item">Contact</a>
+      <div className="navbar-item has-dropdown is-hoverable">
+        <a className="navbar-link is-arrowless">Software</a>
+        <div className="navbar-dropdown is-right">
+          <a className="navbar-item">Resume</a>
+          <a className="navbar-item">LinkedIn</a>
+          <a className="navbar-item">Recipe Book Web App</a>
+          <a className="navbar-item">LiquorBuddy Mobile App</a>
+        </div>
       </div>
+      <a className="navbar-item">Contact</a>
     </div>
   </nav>
 );
