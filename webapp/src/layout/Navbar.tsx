e728ab14ce760078bd76e19747c6f78e3c8fee89
Gregory Chen <gregorychen3@gmail.com>
2019-11-07 22:25:25 -0500

    wire up nav bar to link to routes

--- webapp/src/layout/Navbar.tsx
+++ webapp/src/layout/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 export default () => (
   <nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
       <a className="navbar-item " href="#/">
-        <div className="is-size-4">GREGORY CHEN</div>
+        <Link to="/" className="navbar-item">
+          <div className="is-size-4">GREGORY CHEN</div>
+        </Link>
       </a>
 
       <a
@@ -25,16 +28,26 @@ export default () => (
       <div className="navbar-item has-dropdown is-hoverable">
         <a className="navbar-link is-arrowless">Music</a>
         <div className="navbar-dropdown is-right">
-          <a className="navbar-item">Listen</a>
-          <a className="navbar-item">Schedule</a>
-          <a className="navbar-item">Songbook</a>
-          <a className="navbar-item">Resume</a>
+          <Link to="/music/listen" className="navbar-item">
+            Listen
+          </Link>
+          <Link to="/music/schedule" className="navbar-item">
+            Schedule
+          </Link>
+          <Link to="/music/songbook" className="navbar-item">
+            Songbook
+          </Link>
+          <Link to="/music/resume" className="navbar-item">
+            Resume
+          </Link>
         </div>
       </div>
       <div className="navbar-item has-dropdown is-hoverable">
         <a className="navbar-link is-arrowless">Software</a>
         <div className="navbar-dropdown is-right">
-          <a className="navbar-item">Resume</a>
+          <Link to="/software/resume" className="navbar-item">
+            Resume
+          </Link>
           <a className="navbar-item">LinkedIn</a>
           <a className="navbar-item">Recipe Book Web App</a>
           <a className="navbar-item">LiquorBuddy Mobile App</a>
