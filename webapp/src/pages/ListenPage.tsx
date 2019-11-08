5569e0c254d6efff10b932be1ce58ba2bdf44aec
Gregory Chen <gregorychen3@gmail.com>
2019-11-07 22:57:57 -0500

    impl listen page

--- webapp/src/pages/ListenPage.tsx
+++ webapp/src/pages/ListenPage.tsx
@@ -2,6 +2,55 @@ import React from "react";
 
 export default () => (
   <section className="section">
-    <div className="container">listen page</div>
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <iframe
+            title="I Can Dream, Can't I - Alex Hoffman Quintet 04-29-2018"
+            src="https://www.youtube.com/embed/QSAsV01_DFg?controls=0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            data-frameborder="0"
+            data-allowfullscreen
+            width="100%"
+            height="315"
+          />
+        </div>
+        <div className="column">
+          <iframe
+            title="I Concentrate On You - Alex Hoffman Quintet 04-29-2018"
+            src="https://www.youtube.com/embed/9u3LmdqAwDA?controls=0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            data-frameborder="0"
+            data-allowfullscreen
+            width="100%"
+            height="315"
+          />
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column">
+          <iframe
+            title="Too Late Now - Alex Hoffman Quintet 04-29-2018"
+            src="https://www.youtube.com/embed/56r7TQrSnPo?controls=0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            data-frameborder="0"
+            data-allowfullscreen
+            width="100%"
+            height="315"
+          />
+        </div>
+        <div className="column">
+          <iframe
+            title="If I Could Be With You (One Hour Tonight), James P. Johnson"
+            src="https://www.youtube.com/embed/bdLJmW7sR-4?controls=0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            data-frameborder="0"
+            data-allowfullscreen
+            width="100%"
+            height="315"
+          />
+        </div>
+      </div>
+    </div>
   </section>
 );
