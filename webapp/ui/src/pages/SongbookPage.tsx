e4b12d926d36e33960c4a329d6723e977c80941c
Gregory Chen <gregorychen3@gmail.com>
2019-11-09 18:46:33 -0500

    fetch songs from backend api

--- webapp/ui/src/pages/SongbookPage.tsx
+++ webapp/ui/src/pages/SongbookPage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import apiClient from "../apiClient";
+import { ISongModel } from "../types";
 
 export default () => {
   const folderId = "1XDfbY6K4GzA3Etm-dlwN0-HGhrIICkgw";
+
+  const [songs, setSongs] = useState<ISongModel[]>([]);
+
+  useEffect(() => {
+    const getSongs = async () => {
+      const songs = await apiClient.fetchSongs();
+      setSongs(songs.data);
+    };
+    getSongs();
+  }, []);
 
   return (
     <section className="section">
