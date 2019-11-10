46eed893341d90bd128d3b474c0a51ad44e00696
Gregory Chen <gregorychen3@gmail.com>
2019-11-10 15:00:25 -0500

    remove unused files

--- scripts/rename_files.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-for f in ./{*.pdf,*.sib}; do
-    CORRECTED=`python /Users/gregorychen3/personal-website/misc/correct_filename.py "$f"`
-    echo "$CORRECTED";
-    mv "$f" "$CORRECTED"
-done
-
