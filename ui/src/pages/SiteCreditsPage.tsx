8aa806888d7432577fcab5c22fb05b4bd1596792
Gregory Chen <gregorychen3@gmail.com>
2019-11-10 15:36:25 -0500

    remove punctuation

--- ui/src/pages/SiteCreditsPage.tsx
+++ ui/src/pages/SiteCreditsPage.tsx
@@ -4,13 +4,13 @@ export default () => (
   <section className="section">
     <div className="container">
       <div className="has-text-centered">
-        Photography by Tayla Nebesky.
+        Photography by Tayla Nebesky
         <br />
         <a href="http://www.taylanebesky.com">www.taylanebesky.com</a>
       </div>
       <br />
       <div className="has-text-centered">
-        Site design and implementation by Gregory Chen.
+        Site design and implementation by Gregory Chen
         <br />
         <a href="https://github.com/gregorychen3/personal-website">
           <span className="icon">
