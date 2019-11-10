f561b9bf5f1bcc21e699d97cbbb65d8ece35be47
Gregory Chen <gregorychen3@gmail.com>
2019-11-10 15:29:15 -0500

    link to wite credits page from home page

--- ui/src/pages/HomePage.tsx
+++ ui/src/pages/HomePage.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => (
   <section className="section">
     <div className="container">
       <figure className="image">
         <img src="riverside_church.jpg" alt="At Riverside church" />
+        <Link to="/credits">
+          <div className="is-size-7 has-text-right has-text-black">
+            Site Credits
+          </div>
+        </Link>
       </figure>
     </div>
   </section>
