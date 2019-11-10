1dd0cae7e5e2aec81aba5233c17e7cfd925f0170
Gregory Chen <gregorychen3@gmail.com>
2019-11-10 15:08:24 -0500

    refactor listen page

--- ui/src/pages/ListenPage.tsx
+++ ui/src/pages/ListenPage.tsx
@@ -1,53 +1,49 @@
 import React from "react";
 
+interface YoutubeVideoProps {
+  title: string;
+  src: string;
+}
+const YoutubeVideo = ({ title, src }: YoutubeVideoProps) => (
+  <iframe
+    title={title}
+    src={src}
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    data-frameborder="0"
+    data-allowfullscreen
+    width="100%"
+    height="315"
+  />
+);
+
 export default () => (
   <section className="section">
     <div className="container">
       <div className="columns">
         <div className="column">
-          <iframe
+          <YoutubeVideo
             title="I Can Dream, Can't I - Alex Hoffman Quintet 04-29-2018"
             src="https://www.youtube.com/embed/QSAsV01_DFg?controls=0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            data-frameborder="0"
-            data-allowfullscreen
-            width="100%"
-            height="315"
           />
         </div>
         <div className="column">
-          <iframe
+          <YoutubeVideo
             title="I Concentrate On You - Alex Hoffman Quintet 04-29-2018"
             src="https://www.youtube.com/embed/9u3LmdqAwDA?controls=0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            data-frameborder="0"
-            data-allowfullscreen
-            width="100%"
-            height="315"
           />
         </div>
       </div>
       <div className="columns">
         <div className="column">
-          <iframe
+          <YoutubeVideo
             title="Too Late Now - Alex Hoffman Quintet 04-29-2018"
             src="https://www.youtube.com/embed/56r7TQrSnPo?controls=0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            data-frameborder="0"
-            data-allowfullscreen
-            width="100%"
-            height="315"
           />
         </div>
         <div className="column">
-          <iframe
-            title="If I Could Be With You (One Hour Tonight), James P. Johnson"
+          <YoutubeVideo
+            title="If I Could Be With You, James P. Johnson"
             src="https://www.youtube.com/embed/bdLJmW7sR-4?controls=0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            data-frameborder="0"
-            data-allowfullscreen
-            width="100%"
-            height="315"
           />
         </div>
       </div>
