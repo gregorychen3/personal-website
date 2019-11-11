77632f1b5038d93a63998449e2a5e2aa990fc3c5
Gregory Chen <gchen213@bloomberg.net>
2019-11-11 14:15:23 -0500

    add progress image component partial impl

---
+++ ui/src/components/ProgressiveImage.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const overlayStyles = {
+  position: "absolute",
+  filter: "blur(1px)",
+  transition: "opacity ease-in 1000ms",
+  clipPath: "inset(0)"
+};
+
+interface Props {
+  fullImageSrc: string;
+  overlaySrc: string;
+}
+export default ({ fullImageSrc, overlaySrc }: Props) => {
+  const [isFullImageLoaded, setIsFullImageLoaded] = useState(false);
+  <img src={fullImageSrc} onLoad={() => setIsFullImageLoaded(true)}></img>;
+  <img
+    {...(isFullImageLoaded && { style: { opacity: "0" } })}
+    src={overlaySrc}
+    className={`${overlayStyles}`}
+  />;
+};
