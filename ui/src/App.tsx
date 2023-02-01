2234859ad1612024bfc630a1558dcfaf7fbceed6
Gregory Chen <gregorychen3@gmail.com>
2023-01-31 23:16:54 -0500

    navbar

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,19 +1,11 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { Box, ChakraProvider, Code, Grid, Link, Text, theme, VStack } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { WithSubnavigation } from "./components/Navbar";
+import { Logo } from "./Logo";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <WithSubnavigation />
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
@@ -22,17 +14,11 @@ export const App = () => (
           <Text>
             Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link color="teal.500" href="https://chakra-ui.com" fontSize="2xl" target="_blank" rel="noopener noreferrer">
             Learn Chakra
           </Link>
         </VStack>
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
