c908c4af40dd936d4d2b43b134f5db5ac8a1fe54
Gregory Chen <gregorychen3@gmail.com>
2023-02-04 18:26:05 -0800

    sidenav skel

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,6 @@
-import { Box, Container, CssBaseline, Toolbar } from "@mui/material";
+import { Box, Container, CssBaseline, Grid, Toolbar } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
+import { SideNav } from "./components/SideNav";
 import { HomePage } from "./pages/HomePage";
 
 export function App() {
@@ -9,9 +10,16 @@ export function App() {
       <Box component="main" sx={{ flexGrow: 1, height: "100vh", overflow: "auto" }}>
         <Toolbar />
         <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
+          <Grid container>
+            <Grid item xs={2}>
+              <SideNav />
+            </Grid>
+            <Grid item xs={10}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+              </Routes>
+            </Grid>
+          </Grid>
         </Container>
       </Box>
     </Box>
