db3792f363087309bbb079c6b57fda9c6b08b08e
Gregory Chen <gregorychen3@gmail.com>
2023-02-04 17:33:52 -0800

    button hello world

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@mui/material";
+
 export function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
 }
