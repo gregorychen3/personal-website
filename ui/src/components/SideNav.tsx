71775ff4f89a28f9b0c7558086e2caa5d5dabe3b
Gregory Chen <gregorychen3@gmail.com>
2023-02-05 17:46:35 -0800

    sidenav

--- ui/src/components/SideNav.tsx
+++ ui/src/components/SideNav.tsx
@@ -1,38 +1,41 @@
-import { Stack, styled } from "@mui/material";
+import { Stack } from "@mui/material";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
-
-const Header = styled(Button)(({ theme }) => ({
-  fontFamily: "georgia",
-  textTransform: "none",
-  color: theme.palette.text.primary,
-  pointerEvents: "none",
-  fontWeight: "normal",
-}));
-
-const sxButton = { fontFamily: "georgia", pl: 3, pr: 3, fontWeight: "normal" };
 
 export function SideNav() {
   return (
     <Stack alignItems="flex-start">
-      <Header>home</Header>
-      <Header>music</Header>
-      <Item to="music/resume" label="resume" />
-      <Item to="music/listen" label="listen" />
-      <Item to="music/schedule" label="schedule" />
-      <Item to="music/songbook" label="songbook" />
+      <HeaderItem to="/" label="home" />
 
-      <Header>software</Header>
-      <Item to="software/resume" label="resume" />
-      <Item to="software/linkedin" label="linkedin" />
-      <Item to="software/projects" label="projects" />
+      <HeaderItem to="music/listen" label="music" />
+      <SubItem to="music/listen" label="listen" />
+      <SubItem to="music/schedule" label="schedule" />
+      <SubItem to="music/songbook" label="songbook" />
+      <SubItem to="music/resume" label="resume" />
+
+      <HeaderItem to="software/projects" label="software" />
+      <SubItem to="software/projects" label="projects" />
+      <SubItem to="software/linkedin" label="linkedin" />
+      <SubItem to="software/resume" label="resume" />
     </Stack>
   );
 }
 
-function Item({ to, label }: { to: string; label: string }) {
+const sxHeaderItem = { fontFamily: "georgia", fontWeight: "normal", textTransform: "none", color: "text.primary" };
+
+function HeaderItem({ to, label }: { to: string; label: string }) {
   return (
-    <Button component={Link} to={to} sx={sxButton}>
+    <Button component={Link} to={to} sx={sxHeaderItem}>
+      {label}
+    </Button>
+  );
+}
+
+const sxSubItem = { fontFamily: "georgia", pl: 3, pr: 3, fontWeight: "normal" };
+
+function SubItem({ to, label }: { to: string; label: string }) {
+  return (
+    <Button component={Link} to={to} sx={sxSubItem}>
       {label}
     </Button>
   );
