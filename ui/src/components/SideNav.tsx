fa04d6b00411a67a7cf4ef65b36d418ca41c2232
Gregory Chen <gregorychen3@gmail.com>
2023-02-05 18:43:52 -0800

    active route

--- ui/src/components/SideNav.tsx
+++ ui/src/components/SideNav.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "@mui/material";
 import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export function SideNav() {
   return (
@@ -8,15 +8,15 @@ export function SideNav() {
       <HeaderItem to="/" label="home" />
 
       <HeaderItem to="music/listen" label="music" />
-      <SubItem to="music/listen" label="listen" />
-      <SubItem to="music/schedule" label="schedule" />
-      <SubItem to="music/songbook" label="songbook" />
-      <SubItem to="music/resume" label="resume" />
+      <SubItem to="/music/listen" label="listen" />
+      <SubItem to="/music/schedule" label="schedule" />
+      <SubItem to="/music/songbook" label="songbook" />
+      <SubItem to="/music/resume" label="resume" />
 
       <HeaderItem to="software/projects" label="software" />
-      <SubItem to="software/projects" label="projects" />
-      <SubItem to="software/linkedin" label="linkedin" />
-      <SubItem to="software/resume" label="resume" />
+      <SubItem to="/software/projects" label="projects" />
+      <SubItem to="/software/linkedin" label="linkedin" />
+      <SubItem to="/software/resume" label="resume" />
     </Stack>
   );
 }
@@ -32,10 +32,14 @@ function HeaderItem({ to, label }: { to: string; label: string }) {
 }
 
 const sxSubItem = { fontFamily: "georgia", pl: 3, pr: 3, fontWeight: "normal" };
+const activeColor = "rgba(255, 255, 255, 0.08)";
 
 function SubItem({ to, label }: { to: string; label: string }) {
+  const { pathname } = useLocation();
+  const sx = pathname === to ? { ...sxSubItem, backgroundColor: activeColor } : sxSubItem;
+
   return (
-    <Button component={Link} to={to} sx={sxSubItem} size="small">
+    <Button component={Link} to={to} sx={sx} size="small">
       {label}
     </Button>
   );
