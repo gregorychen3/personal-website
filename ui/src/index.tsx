b4c4f9c065c170420b02e226334369dc3fc0dafc
Gregory Chen <gregorychen3@gmail.com>
2023-02-11 12:23:17 -0800

    uiSlice

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,19 +1,23 @@
 import { ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import { store } from "./app/store";
 import reportWebVitals from "./reportWebVitals";
 import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
