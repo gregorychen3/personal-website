f2a58d1ab1b4e8b8a34d28e98f3ec3e8a37d4d4f
Gregory Chen <gregorychen3@gmail.com>
2023-02-18 17:14:35 -0500

    sidenav layout

--- ui/src/components/SideNav.tsx
+++ ui/src/components/SideNav.tsx
@@ -2,8 +2,6 @@ import { Grid } from "@mui/material";
 import Button from "@mui/material/Button";
 import { Link, useLocation } from "react-router-dom";
 
-const sxIndent = { ml: 3 };
-
 export function SideNav() {
   return (
     <Grid container spacing={0.5}>
@@ -15,36 +13,37 @@ export function SideNav() {
         <HeaderItem to="music/listen" label="music" />
       </Grid>
 
-      <Grid item xs={12} sx={sxIndent}>
+      <Grid item xs={12}>
         <NavItem to="/music/listen" label="listen" />
       </Grid>
-      <Grid item xs={12} sx={sxIndent}>
+      <Grid item xs={12}>
         <NavItem to="/music/schedule" label="schedule" />
       </Grid>
-      <Grid item xs={12} sx={sxIndent}>
+      <Grid item xs={12}>
         <NavItem to="/music/songbook" label="songbook" />
       </Grid>
-      <Grid item xs={12} sx={sxIndent}>
+      <Grid item xs={12}>
         <AnchorItem href={`${process.env.PUBLIC_URL}/music_resume.pdf`} label="resume" />
       </Grid>
 
       <Grid item xs={12}>
         <HeaderItem to="software/projects" label="software" />
       </Grid>
-      <Grid item xs={12} sx={sxIndent}>
+      <Grid item xs={12}>
         <NavItem to="/software/projects" label="projects" />
       </Grid>
-      <Grid item xs={12} sx={sxIndent}>
+      <Grid item xs={12}>
         <AnchorItem href="https://www.linkedin.com/in/gregorychen3" label="linkedin" />
       </Grid>
-      <Grid item xs={12} sx={sxIndent}>
+      <Grid item xs={12}>
         <AnchorItem href={`${process.env.PUBLIC_URL}/software_resume.pdf`} label="resume" />
       </Grid>
     </Grid>
   );
 }
 
-const sxItem = { color: "text.disabled", justifyContent: "flex-start", pl: 1 };
+const sxItem = { color: "text.disabled", justifyContent: "flex-start" };
+const sxIndentedItem = { ...sxItem, pl: 3, pr: 3 };
 const activeColor = "rgba(0, 157, 219, 0.20)";
 
 function HeaderItem({ to, label }: { to: string; label: string }) {
@@ -60,7 +59,7 @@ function HeaderItem({ to, label }: { to: string; label: string }) {
 
 function NavItem({ to, label }: { to: string; label: string }) {
   const { pathname } = useLocation();
-  const sx = pathname === to ? { ...sxItem, backgroundColor: activeColor } : sxItem;
+  const sx = pathname === to ? { ...sxIndentedItem, backgroundColor: activeColor } : sxIndentedItem;
 
   return (
     <Button component={Link} to={to} sx={sx}>
@@ -71,7 +70,7 @@ function NavItem({ to, label }: { to: string; label: string }) {
 
 function AnchorItem({ label, href }: { label: string; href: string }) {
   return (
-    <Button href={href} target="_blank" rel="noopener noreferrer" sx={sxItem}>
+    <Button href={href} target="_blank" rel="noopener noreferrer" sx={sxIndentedItem}>
       {label}
     </Button>
   );
