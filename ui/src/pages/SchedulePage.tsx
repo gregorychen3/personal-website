51b292cba8f87042cb3e9a0293ab7687d3108e71
Gregory Chen <gregorychen3@gmail.com>
2023-09-06 16:11:35 -0400

    fix calendar

--- ui/src/pages/SchedulePage.tsx
+++ ui/src/pages/SchedulePage.tsx
@@ -7,7 +7,7 @@ export function SchedulePage() {
     <div className="container">
       <Iframe
         title="Music Schedule"
-        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23171717&ctz=America%2FNew_York&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=1&showCalendars=0&showTz=1&src=ZTVoNG5vbm8ybzBiNDRhazdrZmZrYjJiYmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%234285F4"
+        src="https://calendar.google.com/calendar/embed?src=toactmj2ehimlgf5b4ru2ppvh4%40group.calendar.google.com&ctz=America%2FNew_York"
         width="100%"
         height="600"
         data-frameborder="0"
