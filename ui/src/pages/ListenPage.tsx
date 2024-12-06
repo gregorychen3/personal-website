b4078b1fc5f54c657314d5d4b773b2c96d532e56
Gregory Chen <gregorychen3@gmail.com>
2024-12-06 13:40:34 -0500

    pages

---
+++ ui/src/pages/ListenPage.tsx
@@ -0,0 +1,45 @@
+import { Grid } from "@mui/material";
+import { YoutubeVideo } from "../components/YoutubeVideo";
+
+export function ListenPage() {
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12} sm={6}>
+        <YoutubeVideo
+          title="Bye Bye Baby"
+          src="https://www.youtube.com/embed/VlYeIZZwdqc"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <YoutubeVideo
+          title="Aaron Johnson Quintet 07-03-2022"
+          src="https://www.youtube.com/embed/bkfCbIMIqA4?start=88"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <YoutubeVideo
+          title="Alex Hoffman Quintet - Smalls 10/31/2021 Set 1"
+          src="https://www.youtube.com/embed/D9ulpdTD6hY"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <YoutubeVideo
+          title="Alex Hoffman Quintet - Smalls 10/31/2021 Set 2"
+          src="https://www.youtube.com/embed/wkXdCbxZD2w"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <YoutubeVideo
+          title="I Concentrate On You - Alex Hoffman Quintet 04-29-2018"
+          src="https://www.youtube.com/embed/9u3LmdqAwDA"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <YoutubeVideo
+          title="Too Late Now - Alex Hoffman Quintet 04-29-2018"
+          src="https://www.youtube.com/embed/56r7TQrSnPo"
+        />
+      </Grid>
+    </Grid>
+  );
+}
---
+++ ui/src/pages/SongbookPage.tsx
@@ -0,0 +1,89 @@
+import { CardActionArea, Grid } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { apiClient, Song } from "../apiClient";
+import { setShowLoading } from "../features/ui/uiSlice";
+
+export function SongbookPage() {
+  const d = useDispatch();
+
+  const [songs, setSongs] = useState<Song[]>([]);
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChanged = (e: React.ChangeEvent<HTMLInputElement>) => setSearchText(e.target.value);
+
+  useEffect(() => {
+    const getSongs = async () => {
+      d(setShowLoading(true));
+
+      const songs = await apiClient.fetchSongs();
+      console.log(songs);
+      setSongs(songs);
+
+      d(setShowLoading(false));
+    };
+
+    getSongs();
+
+    return () => {
+      d(setShowLoading(false));
+    };
+  }, [d]);
+
+  const filterFn = (s: Song) => {
+    if (!searchText) {
+      return true;
+    }
+
+    const want = searchText.toLowerCase();
+
+    if (s.name.toLowerCase().includes(want)) {
+      return true;
+    }
+
+    if (s.year?.toString().includes(want)) {
+      return true;
+    }
+
+    if (s.authors.find((author) => author.toLowerCase().includes(want))) {
+      return true;
+    }
+
+    return false;
+  };
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <TextField label="Search songs" variant="filled" fullWidth onChange={handleSearchChanged} />
+      </Grid>
+      {songs.filter(filterFn).map((s) => (
+        <Grid item xs={12}>
+          <SongCard song={s} key={s.id} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+const SongCard = ({ song }: { song: Song }) => {
+  const handleClick = () => window.open(`https://drive.google.com/open?id=${song.id}`, "_blank", "noreferrer");
+
+  return (
+    <Card onClick={handleClick}>
+      <CardActionArea>
+        <CardContent>
+          <Typography variant="body2">{song.name}</Typography>
+          <Typography variant="body2" color="text.disabled">
+            <div>{song.year}</div>
+            <div>{song.authors.join(", ")}</div>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
