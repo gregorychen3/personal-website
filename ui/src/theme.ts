dc14d0e2291c1f0289f1edab86388d3569e3520c
Gregory Chen <gregorychen3@gmail.com>
2024-12-06 13:25:37 -0500

    theme.ts

---
+++ ui/src/theme.ts
@@ -0,0 +1,28 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: { main: "#009DDB" },
+    secondary: { main: "#F09000" },
+    error: { main: "#EC1600" },
+    warning: { main: "#EBA300" },
+    success: { main: "#00C78E" },
+    background: { default: "black", paper: "#171717" },
+    mode: "dark",
+  },
+  typography: {
+    fontSize: 18,
+    fontFamily: ["gill sans", "sans-serif"].join(","),
+    button: {
+      textTransform: "none",
+    },
+  },
+});
+
+//theme.palette.primary.main = theme.palette.text.disabled;
+
+theme.components = {
+  MuiTextField: {
+    defaultProps: {},
+  },
+};
