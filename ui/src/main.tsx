a6ee204b89b90eac404b4e75676614507b40efed
Gregory Chen <gregorychen3@gmail.com>
2024-12-06 13:39:24 -0500

    main.tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ThemeProvider } from "@mui/material";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router";
+import { QueryParamProvider } from "use-query-params";
+import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6";
+import App from "./App.tsx";
+import { store } from "./app/store";
+import "./index.css";
+import { theme } from "./theme";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <QueryParamProvider adapter={ReactRouter6Adapter}>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </QueryParamProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
-)
+);
